bab26854b0032c6b3cc0f69632a9ddb44ec17bee
Anish Dharam <anish_dharam@brown.edu>
2024-12-01 14:34:21 -0500

    changed example objects in main.py

--- main.py
+++ main.py
@@ -10,31 +10,29 @@ def main():
 
 #world
     world: HittableList = HittableList([])
-    #anish example
-    # world.add(Sphere(Point3(0, 0, -1), 0.5))
-    # world.add(Sphere(Point3(2, 0.1, -3), 0.4))
-    # world.add(Sphere(Point3(0, -210.5, -3), 200))
-    # world.add(Cube(Point3(-1.0, 1.0, -1.5), 0.4))
-    # world.add(Cube(Point3(4.0, -3.0, -5.5), 1.4))
-    
-    # #book original example + cube
-    # world.add(Sphere(Point3(0,0,-1), 0.5, Lambertian(Color(0.8, 1, 0.9))))
-    # world.add(Cube(Point3(-1.0, 0.03, -1), 0.4, Lambertian(Color(0.2, 0.1, 0.3))))
-    # world.add(Sphere(Point3(0,-100.5,-1), 100, Lambertian(Color(0.5, 0.4, 0.55))))
 
     #book metal example
     material_ground = Lambertian(Color(0.8, 0.8, 0.0))
     material_center = Lambertian(Color(0.1, 0.2, 0.5))
     material_left   = Metal(Color(0.8, 0.8, 0.8))
     material_right  = Metal(Color(0.8, 0.6, 0.2))
+    # material_cube   = Lambertian(Color(0.7, 0.91, 0.95))
 
+    #anish example
+    # world.add(Sphere(Point3(0, 0, -1), 0.5, material_left))
+    # world.add(Sphere(Point3(2, 0.1, -3), 0.4, material_right))
+    # world.add(Sphere(Point3(0, -210.5, -3), 200, material_ground))
+    # world.add(Cube(Point3(-1.0, 1.0, -1.5), 0.4, material_center))
+    # world.add(Cube(Point3(4.0, -3.0, -5.5), 1.4, material_cube))
+    
+    #book metal example with cube
     world.add(Sphere(Point3( 0.0, -100.5, -1.0), 100.0, material_ground))
     world.add(Sphere(Point3( 0.0,    0.0, -1.2),   0.5, material_center))
-    world.add(Sphere(Point3(-1.0,    0.0, -1.0),   0.5, material_left))
+    world.add(Cube(Point3(-1.0,    0.0, -0.8),   0.5, material_left))
     world.add(Sphere(Point3( 1.0,    0.0, -1.0),   0.5, material_right))
 
 #camera
-    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=400, samples_per_pixel=100, max_depth=50)
+    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=400, samples_per_pixel=5, max_depth=50)
     cam.render(world)
     return
 
