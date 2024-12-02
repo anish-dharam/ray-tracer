7e51d7e6e2d8c37a850fcc9b1ebdc19a1f48e2cf
Anish Dharam <anish_dharam@brown.edu>
2024-12-02 00:44:08 -0500

    project settings before final (book) render

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from vec3 import Vec3, Point3, Color
+from vec3 import Vec3, Point3, Color, random_vec
 from hittable_list import HittableList
 from sphere import Sphere
 from cube import Cube
 from camera import Camera
 from material import Lambertian, Metal, Dielectric
+import random
 
 
 def main():
@@ -11,37 +12,46 @@ def main():
 #world
     world: HittableList = HittableList([])
 
-    #book metal example
-    material_ground = Lambertian(Color(0.6, 0.8, 0.2))
-    # material_ground = Lambertian(Color(0.8, 0.8, 0.0))
-    material_center = Lambertian(Color(0.5, 0.1, 0.2))
-    # material_center = Lambertian(Color(0.1, 0.2, 0.5))
-    material_left_glass   = Dielectric(1.5)
-    material_bubble   = Dielectric(1/1.5)
-    material_left_metal   = Metal(Color(0.8, 0.8, 0.8), 0) #reduced fuzz from 0.3 to 0
-    material_right  = Metal(Color(0.8, 0.6, 0.2), 0.3) #reduced fuzz from 1 to 0.3
-    material_cube   = Lambertian(Color(0.7, 0.91, 0.95))
+    ground_material = Lambertian(Color(0.5, 0.5, 0.5))
+    world.add(Sphere(Point3(0,-1000,0), 1000, ground_material))
 
-    #anish example
-    world.add(Sphere(Point3(0, 0, -1), 0.5, material_left_metal))
-    world.add(Sphere(Point3(2, 0.1, -3), 0.4, material_right))
-    world.add(Sphere(Point3(0, -210.5, -3), 200, material_ground))
-    world.add(Cube(Point3(-1.0, 1.0, -1.5), 0.4, material_center))
-    world.add(Cube(Point3(4.0, -3.0, -5.5), 1.4, material_left_glass))
+    for a in range(-6, 6):
+        for b in range(-6, 6):
+            choose_mat = random.uniform(0, 1)
+            center = Point3(a + 0.9 * random.uniform(0, 1), 0.2, b + 0.9* random.uniform(0, 1))
+
+            if ((center - Point3(4, 0.2, 0)).length() > 0.9):
+                if (choose_mat < 0.8):
+                    albedo = random_vec() * random_vec()
+                    sphere_material = Lambertian(albedo)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                elif (choose_mat < 0.95):
+                    albedo = random_vec(0.5, 1)
+                    fuzz = random.uniform(0, 0.5)
+                    sphere_material = Metal(albedo, fuzz)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                else:
+                    sphere_material = Dielectric(1.5)
+                    world.add(Sphere(center, 0.2, sphere_material))
+            
+        
     
-    # #book metal example with cube
-    # world.add(Sphere(Point3( 0.0, -100.5, -1.0), 100.0, material_ground))
-    # world.add(Sphere(Point3( 0.0,    0.0, -1.2),   0.5, material_center))
-    # # world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .5, material_left_glass))
-    # # world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .4, material_bubble))
-    # world.add(Cube(Point3(-1.0,    -0.1, -1.2),   0.37, material_left_metal))
-    # world.add(Sphere(Point3( 1.0,    0.0, -1.0),   0.5, material_right))
+
+    material1 = Dielectric(1.5)
+    world.add(Sphere(Point3(0, 1, 0), 1.0, material1))
+
+    material2 = Lambertian(Color(0.4, 0.2, 0.1))
+    world.add(Sphere(Point3(-4, 1, 0), 1.0, material2))
+
+    material3 = Metal(Color(0.7, 0.6, 0.5), 0.0)
+    world.add(Sphere(Point3(4, 1, 0), 1.0, material3))
+
 
 
 #camera
-    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=400, samples_per_pixel=500, max_depth=50, 
-                 vfov=90, look_from=Point3(0, 0, 0), look_at=Point3(0, 0, -1), view_up=Vec3(0, 1, 0),
-                 defocus_angle=0.0001, focus_dist=0.0001)
+    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=1200, samples_per_pixel=300, max_depth=50, 
+                 vfov=20, look_from=Point3(13, 2, 3), look_at=Point3(0, 0, 0), view_up=Vec3(0, 1, 0),
+                 defocus_angle=0.6, focus_dist=10)
     cam.render(world)
     return
 
