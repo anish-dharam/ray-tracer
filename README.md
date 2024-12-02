66a198b7242089449febf0435072a9a2e9f97e07
Anish Dharam <anish_dharam@brown.edu>
2024-12-01 21:12:01 -0500

    project settings before rendering floating objects

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 IN PROGRESS
 
-Written with:
+Written with "Ray Tracing in One Weekend"
 https://raytracing.github.io/books/RayTracingInOneWeekend.html#outputanimage
--- main.py
+++ main.py
@@ -18,29 +18,29 @@ def main():
     # material_center = Lambertian(Color(0.1, 0.2, 0.5))
     material_left_glass   = Dielectric(1.5)
     material_bubble   = Dielectric(1/1.5)
-    material_left_metal   = Metal(Color(0.8, 0.8, 0.8), 0.3)
-    material_right  = Metal(Color(0.8, 0.6, 0.2), 1.0)
-    # material_cube   = Lambertian(Color(0.7, 0.91, 0.95))
+    material_left_metal   = Metal(Color(0.8, 0.8, 0.8), 0) #reduced fuzz from 0.3 to 0
+    material_right  = Metal(Color(0.8, 0.6, 0.2), 0.3) #reduced fuzz from 1 to 0.3
+    material_cube   = Lambertian(Color(0.7, 0.91, 0.95))
 
     #anish example
-    # world.add(Sphere(Point3(0, 0, -1), 0.5, material_left))
-    # world.add(Sphere(Point3(2, 0.1, -3), 0.4, material_right))
-    # world.add(Sphere(Point3(0, -210.5, -3), 200, material_ground))
-    # world.add(Cube(Point3(-1.0, 1.0, -1.5), 0.4, material_center))
-    # world.add(Cube(Point3(4.0, -3.0, -5.5), 1.4, material_cube))
+    world.add(Sphere(Point3(0, 0, -1), 0.5, material_left_metal))
+    world.add(Sphere(Point3(2, 0.1, -3), 0.4, material_right))
+    world.add(Sphere(Point3(0, -210.5, -3), 200, material_ground))
+    world.add(Cube(Point3(-1.0, 1.0, -1.5), 0.4, material_center))
+    world.add(Cube(Point3(4.0, -3.0, -5.5), 1.4, material_left_glass))
     
-    #book metal example with cube
-    world.add(Sphere(Point3( 0.0, -100.5, -1.0), 100.0, material_ground))
-    world.add(Sphere(Point3( 0.0,    0.0, -1.2),   0.5, material_center))
-    world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .5, material_left_glass))
-    world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .4, material_bubble))
+    # #book metal example with cube
+    # world.add(Sphere(Point3( 0.0, -100.5, -1.0), 100.0, material_ground))
+    # world.add(Sphere(Point3( 0.0,    0.0, -1.2),   0.5, material_center))
+    # # world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .5, material_left_glass))
+    # # world.add(Sphere(Point3(-1.0,    0.0, -1.0),   .4, material_bubble))
     # world.add(Cube(Point3(-1.0,    -0.1, -1.2),   0.37, material_left_metal))
-    world.add(Sphere(Point3( 1.0,    0.0, -1.0),   0.5, material_right))
+    # world.add(Sphere(Point3( 1.0,    0.0, -1.0),   0.5, material_right))
 
 
 #camera
-    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=400, samples_per_pixel=5, max_depth=50, 
-                 vfov=20, look_from=Point3(-2, 2, 1), look_at=Point3(0, 0, -1), view_up=Vec3(0, 1, 0),
+    cam = Camera(aspect_ratio=16.0 / 9.0, image_width=400, samples_per_pixel=500, max_depth=50, 
+                 vfov=90, look_from=Point3(0, 0, 0), look_at=Point3(0, 0, -1), view_up=Vec3(0, 1, 0),
                  defocus_angle=0.0001, focus_dist=0.0001)
     cam.render(world)
     return
